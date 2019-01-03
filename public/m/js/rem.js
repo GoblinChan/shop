b7fee240a15a069992966bb9b8e9586813eb8cc7
Bunana <fengfeng1995@vip.qq.com>
2019-01-03 09:32:56 +0800

    first-day ver.1.0.1 (alter base.less and index.html from view)

--- public/m/js/rem.js
+++ public/m/js/rem.js
@@ -1,17 +1,22 @@
 /*
-** Create by Bunana on 2019/1/2
-*/
+ ** Create by Bunana on 2019/1/2
+ */
 setHtmlFontSize();
-function setHtmlFontSize (){
+
+function setHtmlFontSize() {
     // 设计稿大小
     var designWidth = 750;
-// 设计稿根元素大小
+    // 设计稿根元素大小
     var designFontSize = 200;
-// 获取当前屏幕宽度
+    // 获取当前屏幕宽度
     var windowWidth = document.documentElement.offsetWidth;
-// 计算当前屏幕根元素大小
-    var nowFontSize = windowWidth/(designWidth/designFontSize)
-// 设置到当前html元素的fontsize;
-    document.documentElement.style.fontSize = nowFontSize+'px';
+    // 计算当前屏幕根元素大小
+    var nowFontSize = windowWidth / (designWidth / designFontSize)
+    // 设置最大font-size为200px;
+    if (nowFontSize > 200) {
+        nowFontSize = 200;
+    }
+    // 设置到当前html元素的fontsize;
+    document.documentElement.style.fontSize = nowFontSize + 'px';
 }
 window.addEventListener('resize', setHtmlFontSize);
